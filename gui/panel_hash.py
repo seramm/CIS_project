593769e67bcb99ec22300b372156748c644a9dae
seramm <seram@tuta.com>
2026-05-27 02:37:42 +0300

    feat: add second hash generation round

--- gui/panel_hash.py
+++ gui/panel_hash.py
@@ -16,17 +16,36 @@ class PanelHashing(tk.Frame):
         self.btn = tk.Button(self, text="Generate Hash", command=self.process_hash)
         self.btn.pack(pady=5)
 
-        self.result = tk.Entry(self, width=65, state="readonly")
-        self.result.pack(pady=5)
+        self.frame_result = tk.LabelFrame(
+            self, text="Calculated SHA256 hashes", font=("Ubuntu Mono", 9, "bold")
+        )
+        self.frame_result.pack(pady=10, padx=10, fill="x")
+
+        self.lbl_result_1 = tk.Label(self.frame_result, text="1st SHA256 hash")
+        self.lbl_result_1.pack(pady=2)
+        self.result_1 = tk.Entry(self.frame_result, width=65, state="readonly")
+        self.result_1.pack(pady=5)
+
+        self.lbl_result_2 = tk.Label(self.frame_result, text="2nd SHA256 hash")
+        self.lbl_result_2.pack(pady=2)
+        self.result_2 = tk.Entry(self.frame_result, width=65, state="readonly")
+        self.result_2.pack(pady=5)
 
     def process_hash(self):
         msg = self.entry.get()
         try:
-            final_hash = gen_sha256(msg)
+            final_hash_1 = gen_sha256(msg)
+            final_hash_2 = gen_sha256(msg)
 
-            self.result.config(state="normal")
-            self.result.delete(0, tk.END)
-            self.result.insert(0, final_hash)
-            self.result.config(state="readonly")
+            self.result_1.config(state="normal")
+            self.result_1.delete(0, tk.END)
+            self.result_1.insert(0, final_hash_1)
+            self.result_1.config(state="readonly")
+
+            self.result_2.config(state="normal")
+            self.result_2.delete(0, tk.END)
+            self.result_2.insert(0, final_hash_2)
+            self.result_2.config(state="readonly")
+
         except ValueError as e:
             messagebox.showwarning("Error", str(e))
