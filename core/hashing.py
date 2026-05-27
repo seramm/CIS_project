9adb6c35a2f1352267fa27b665bf79c00a5d85de
seramm <seram@tuta.com>
2026-05-27 18:45:43 +0300

    chore(core): define hash input encoding type

--- core/hashing.py
+++ core/hashing.py
@@ -4,4 +4,4 @@ import hashlib
 def gen_sha256(msg: str) -> str:
     if not msg:
         raise ValueError("Missing message to hash")
-    return hashlib.sha256(msg.encode()).hexdigest()
+    return hashlib.sha256(msg.encode("utf-8")).hexdigest()
